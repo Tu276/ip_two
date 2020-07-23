e0265a26e013dfd4a921c15b20fb14eda1be4b7a
Tu276 <tu276nathan@gmail.com>
2020-07-23 17:56:49 +0300

    add readme.md edits

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1, number2);
+var temperature = 10;
+var raining  = true;
+if (temperature < 0) {
+  console.log("Dint'freeze out there!");
+}
+  else if (temperature < 15 && raining === true){
+    console.log("Dont forget jacket!");
+}
+  else if (temperature <25){
+    console.log("Have anice day :)");
+}
+  else{
+    console.log("keep cool today!");
+}
